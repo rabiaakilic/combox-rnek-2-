36c0afe22acb6a8e990951c87994eee06f02c61f
rabiaakilic <147382992+rabiaakilic@users.noreply.github.com>
2023-11-20 12:08:56 +0300

    Add files via upload

---
+++ combox örnek 2 _ 1359/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace combox_örnek_2___1359
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtAranan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.Location = new System.Drawing.Point(30, 96);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(213, 316);
+            this.lbSayilar.TabIndex = 0;
+            this.lbSayilar.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(157, 57);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtAranan
+            // 
+            this.txtAranan.Location = new System.Drawing.Point(30, 57);
+            this.txtAranan.Name = "txtAranan";
+            this.txtAranan.Size = new System.Drawing.Size(100, 20);
+            this.txtAranan.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 450);
+            this.Controls.Add(this.txtAranan);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.lbSayilar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtAranan;
+    }
+}
+
---
+++ combox örnek 2 _ 1359/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combox_örnek_2___1359
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            int aranan = Convert.ToInt32(txtAranan.Text);
+            bool bulundu = false;
+            for (int i = 0; i < 20; i++)
+            {
+                int sayi = Convert.ToInt32(lbSayilar.Items[i]);
+
+                if(sayi == aranan)
+                {
+                    bulundu = true;
+                    break;//bu konut döngüden çıkarır...
+                }
+            } 
+
+            if (bulundu)
+            {
+                MessageBox.Show("aranan sayı bulundu");
+            }
+            else
+            {
+                MessageBox.Show("aranan sayı yok");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int i =0; i < 20; i++)
+            {
+                //random .next(1,101 arasında rastgele sayı olustur  
+                lbSayilar.Items.Add(random.Next(1,101));
+            }
+        }
+    }
+}
